f7edd33f4d15ee5814a68bd1e1246fdb47c0d82b
Yeshwanth <yeshwanth840@gmail.com>
2025-06-05 19:12:14 +0530

    updated

---
+++ p2/script.js
@@ -0,0 +1,25 @@
+const showLogin = () => {
+    let str=`
+    <div class='App-Container'>
+    <h1>Login Form</h1>
+    <p><input type="text" id="txtEmail"></p>
+    <p><input type="password" id="txtPass"></p>
+    <p><button>Log In</button></p>
+    <p><button onclick='showRegister()'>Create Account</button></p>
+    </div>
+    `
+    root.innerHTML = str
+}
+
+const showRegister = () => {
+      let str=`
+    <h1>Register Form</h1>
+    <p><input type="text" id="txtName"></p>
+     <p><input type="text" id="txtEmail"></p>
+    <p><input type="password" id="txtPass"></p>
+    <button>Register</button>
+    <p><button onclick='showLogin()'>Already a member</button></p>
+    
+    `
+    root.innerHTML = str
+}
